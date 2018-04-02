2277fd1c6b4c4d84f123ae069cfe169a126191e5
Adrian Reyes <Adrian.Reyes@adidas.com>
2018-04-02 19:33:44 +0200

    guard safes

--- scala/src/main/scala/tennis/TennisGame1.scala
+++ scala/src/main/scala/tennis/TennisGame1.scala
@@ -1,56 +1,58 @@
 package tennis
 
 
-class TennisGame1 (val player1Name : String, val player2Name : String) extends TennisGame {
+class TennisGame1(val player1Name: String, val player2Name: String) extends TennisGame {
+
   import TennisGame1._
 
   var m_score1: Int = 0
   var m_score2: Int = 0
 
-  def wonPoint(playerName : String) {
-          if (isPlayerOne(playerName))
-              m_score1 += 1
-          else
-              m_score2 += 1
-      }
+  def wonPoint(playerName: String) {
+    if (isPlayerOne(playerName))
+      m_score1 += 1
+    else
+      m_score2 += 1
+  }
 
-  def calculateScore() : String = {
-      var score : String = ""
-      var tempScore=0
-      if (isATie)
-      {
-        score = m_score1 match {
-              case 0 =>  "Love-All"
-              case 1 => "Fifteen-All"
-              case 2 => "Thirty-All"
-              case _ => "Deuce"
+  def calculateScore(): String = {
+    var score: String = ""
+    var tempScore = 0
 
-          }
+    if (isATie) {
+      return m_score1 match {
+        case 0 => "Love-All"
+        case 1 => "Fifteen-All"
+        case 2 => "Thirty-All"
+        case _ => "Deuce"
       }
-      else if (aPlayerHasScoredFortyOrMore)
-      {
-          val minusResult = m_score1-m_score2
-          if (minusResult==1) score ="Advantage player1"
-          else if (minusResult == -1) score ="Advantage player2"
-          else if (minusResult>=2) score = "Win for player1"
-          else score ="Win for player2"
+    }
+
+    if (aPlayerHasScoredFortyOrMore) {
+      val minusResult = m_score1 - m_score2
+      if (minusResult == 1) score = "Advantage player1"
+      else if (minusResult == -1) score = "Advantage player2"
+      else if (minusResult >= 2) score = "Win for player1"
+      else score = "Win for player2"
+
+      return score
+    }
+
+    for (i <- 1 until 3 by 1) {
+      if (i == 1) tempScore = m_score1
+      else {
+        score += "-";
+        tempScore = m_score2;
       }
-      else
-      {
-          for ( i<- 1 until 3 by 1)
-          {
-              if (i==1) tempScore = m_score1
-              else { score+="-"; tempScore = m_score2;}
-              val tempScore2 = tempScore match {
-                  case 0 => "Love"
-                  case 1 => "Fifteen"
-                  case 2 => "Thirty"
-                  case 3 => "Forty"
-              }
-            score += tempScore2
-          }
+      val tempScore2 = tempScore match {
+        case 0 => "Love"
+        case 1 => "Fifteen"
+        case 2 => "Thirty"
+        case 3 => "Forty"
       }
-    return score
+      score += tempScore2
+    }
+    score
   }
 
   private def aPlayerHasScoredFortyOrMore = {
